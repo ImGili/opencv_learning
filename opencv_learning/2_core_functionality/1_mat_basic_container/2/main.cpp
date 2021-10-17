ae2da10b15dff78075f7f123fb113e1f67eaa90d
xhguleixin123 <18017158729@163.com>
2021-10-17 15:38:43 +0800

    ✨ feat: 矩阵输出格式

--- opencv_learning/2_core_functionality/1_mat_basic_container/2/main.cpp
+++ opencv_learning/2_core_functionality/1_mat_basic_container/2/main.cpp
@@ -3,18 +3,14 @@
 using namespace cv;
 int main(int argc, char** argv )
 {
-    Mat M(2, 2, CV_8UC3, Scalar(0, 0, 255));
-    std::cout << "M = " << std::endl << M << std::endl;
+    Mat R = Mat(3, 2, CV_8UC3);
+    randu(R, Scalar::all(0), Scalar::all(255));
 
-    M.create(4,4, CV_8UC(2));
-    std::cout << "M = " << std::endl << M << std::endl;
-
-    Mat E = Mat::eye(4, 4, CV_64F);
-    Mat O = Mat::ones(2, 2, CV_32F);
-    Mat Z = Mat::zeros(3, 3, CV_8UC1);
-    std::cout << "E = " << std::endl << E << std::endl;
-    std::cout << "O = " << std::endl << O << std::endl;
-    std::cout << "Z = " << std::endl << Z << std::endl;
+    std::cout << "R(default) = " << std::endl << R << std::endl;
+    std::cout << "R(python) = " << std::endl << format(R, Formatter::FMT_PYTHON) << std::endl;
+    std::cout << "R(csv) = " << std::endl << format(R, Formatter::FMT_CSV) << std::endl;
+    std::cout << "R(numpy) = " << std::endl << format(R, Formatter::FMT_NUMPY) << std::endl;
+    std::cout << "R(c) = " << std::endl << format(R, Formatter::FMT_C) << std::endl;
 
     return 0;
 }
